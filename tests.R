5e13a407891d240885fe5b49bd25a72f85d0deda
Michael Young <myoung3@deohs-mesa03.deohs.washington.edu>
2019-09-19 18:28:22 -0700

    intermediate changes. adding expansion (slow) averaging function for comparison/testing

--- tests.R
+++ tests.R
@@ -7,12 +7,8 @@ set.seed(180)
 a_start_date <- seq(structure(10590, class = "Date"),
                     structure(17163, class = "Date"),by=7)
 
-
-a_end_date <- seq(structure(10596, class = "Date"),
-                    structure(17169, class = "Date"),by=7)
-
 a <- CJ(id1=1:3,id2=1:100, start_date=a_start_date)
-a <- merge(data.table(start_date=a_start_date,end_date=a_end_date),a, by="start_date")
+a[, end_date:=start_date+6]
 a[, value1:=rnorm(nrow(a))]
 a[, value2:=rnorm(nrow(a))]
 
@@ -20,9 +16,72 @@ b <- data.table(start_date=as.Date(paste0(1999:2017,"-01-01")),
                   end_date=as.Date(paste0(1999:2017,"-12-31")))
 
 
-interval_weighted_avg_f(x=a,y=b,interval_vars=c("start_date","end_date"),
+###two groups in x,two values####
+
+q <- interval_weighted_avg_f(x=a,y=b,interval_vars=c("start_date","end_date"),
                         value_vars=c("value1","value2"),
                         group_vars=c("id1","id2"))
+
+  #simple example:
+a <- CJ(id=1:2,id2=1:2,start=seq(1,50,by=7))
+a[, end:=start+6]
+a[, value:=rbinom(nrow(a),5,prob=.5)]
+b <- data.table(start=seq(0,42,by=14))
+b[,end:=start+13]
+
+
+q1 <- interval_weighted_avg_f(x=a,
+                             y=b,interval_vars=c("start","end"),
+                             group_vars=c("id","id2"),
+                             value_vars=c("value"))
+
+
+q2 <- interval_weighted_avg_slow_f(x=a,
+                             y=b,interval_vars=c("start","end"),
+                             group_vars=c("id","id2"),
+                             value_vars=c("value"))
+
+#no group, only 1 value ###
+
+q <- interval_weighted_avg_f(x=data.table(start_date=a_start_date,
+                                          end_date=a_end_date,
+                                          value1=rnorm(length(a_start_date))),
+                             y=b,interval_vars=c("start_date","end_date"),
+                             value_vars=c("value1"))
+
+
+
+
+###averaging intervals shorter than observed period ####
+b2 <- data.table(start_date=seq(as.Date(paste0(1999,"-01-01")),
+                                as.Date(paste0(2017,"-12-31")),by=1),
+                end_date=seq(as.Date(paste0(1999,"-01-01")),
+                             as.Date(paste0(2018,"-01-31")),by=1))
+
+##averaging intervals shorter than observed period includes dates not in observed period at all ####
+ #
+b2 <- data.table(start_date=seq(as.Date(paste0(1999,"-01-01")),
+                                as.Date(paste0(2018,"-1-31")),by=1),
+                 end_date=seq(as.Date(paste0(1999,"-01-01")),
+                              as.Date(paste0(2018,"-01-31")),by=1))
+
+q <- interval_weighted_avg_f(x=data.table(start_date=a_start_date,
+                                          end_date=a_end_date,
+                                          value1=rnorm(length(a_start_date))),
+                             y=b2,interval_vars=c("start_date","end_date"),
+                             group_vars=NULL,
+                             value_vars=c("value1"))
+
+
+
+#1 groups in both x AND y with different desired averagin periods for each group
+ #(e.g., different desired averaging period for each group))
+
+#two groups in x and one group in y
+
+#irregular length observation periods that are not necessarily adjoining 
+
+#periods that have overlaps--this should return an error
 
 #misspecified order of interval_vars should return an error:
 stopifnot(tryCatch(
@@ -30,17 +89,3 @@ stopifnot(tryCatch(
                           value_vars=c("value1","value2"),
                           group_vars=c("id1","id2")),
   error=function(x){TRUE}))
-
-#no groups
-
-#1 group
-
-#2 groups
-
-#averaging intervals shorter than observed period
-
-#2 groups, 3 groups (in x AND y (e.g., different desired averaging period for each group))
-
-#irregular length observation periods that are not necessarily adjoining 
-
-#periods that have overlaps--this should return an error
--- timeperiod_functions.R
+++ timeperiod_functions.R
@@ -1,5 +1,7 @@
 library(data.table)
 
+
+
 #function to take non-overlapping time-integrated averages observed over a defined period
 #and average those observations up (or down) to a specified schedule
 #this schedule does not necessarily need to align to the periods
@@ -14,20 +16,36 @@ library(data.table)
 #and allow specification of what percentage of time within the specified schedule needs to be 
  #observed in order to not return NA. default is 100
 
+#note that for periods in y not overlapping with any periods in x, no rows will be returned
+
 #interaval_vars is a length-2 vector of shared column names
  #in x an y corresponding to the start and end dates
-#group_vars is a character vector of column names in x (and possibly shared in y) 
+#group_vars is a character vector of column names in x 
  #that designate groups
+
+
+#by default, the function ensures that periods are non-overlapping in x and y
+#this is slow, but is a necessary condition of this function
+#if you're sure your intervals are non-overlapping you can skip this
+ #check by specifying skip_overlap_check=TRUE
+
 interval_weighted_avg_f <- function(x, y,interval_vars,value_vars, group_vars=NULL,
                                     required_percentage=100,skip_overlap_check=FALSE){
-  y <- copy(y)
+  EVAL <- function(...)eval(parse(text=paste0(...)))
+  
+  ###error checks:
   #stop if there are variables specified in both groups and interval_vars
   stopifnot(length(intersect(interval_vars,group_vars))==0)
   
-  #stop if group vars are not columns in y
   if(!is.null(group_vars)){
+    #stop if group vars are not columns in x
     stopifnot(all(group_vars %in% names(x)))
+    
+    #stop if group vars exist in y
+    if(any(group_vars %in% names(y))){stop("grouping vars in y not implemented")}
   }
+  
+  
   
   #stop if start_dates are before end dats
   stopifnot(x[, sum(.SD[[2]]-.SD[[1]] <0)==0,.SDcols=interval_vars])
@@ -36,7 +54,7 @@ interval_weighted_avg_f <- function(x, y,interval_vars,value_vars, group_vars=NU
   #set keys for testing if there are overlaps
   #groups do not need to be in x but they do need to be in y
   if(!skip_overlap_check){
-    setkeyv(y,c(intersect(group_vars,names(y)), interval_vars))
+    setkeyv(y,interval_vars)
     setkeyv(x,c(group_vars,interval_vars))
     
     #stop if there are overlapping periods within groups: 
@@ -44,97 +62,130 @@ interval_weighted_avg_f <- function(x, y,interval_vars,value_vars, group_vars=NU
     stopifnot(nrow(foverlaps(x,x))==nrow(x))  
     print(paste(Sys.time(),"passed test to determine whether data is non-overlapping"))
   }else{
-    print("skipping test to determine whether data is non-overlapping")
+    print("Caution: skipping test to determine whether data is non-overlapping")
   }
   
-  print(paste(Sys.time(),"begin setting final y keys"))
-  if(any(group_vars %in% names(y))){
-    setkeyv(y,c(intersect(group_vars,names(y)), interval_vars))
-  }else{
-    setkeyv(y,interval_vars)
-  }
-  print(paste(Sys.time(),"setting final y keys complete"))
   
+  #create column names for variables that will be created in this function
+    #create a character vector representing the 
+      #second set of column names for start and end that will be created  
+      #once x and y are merged via foverlaps
+      #the ones prefixed with i. are from x 
+    xinterval_vars <- paste0("i.",interval_vars)
+    
+    #find a name for a new  "duration" variable that's not already taken
+    dur <- "duration"
+    while(dur%in% names(x)|dur%in% names(y)){
+      dur <- paste0(".",dur)
+    }
+    #find a name for a new  "xduration" and "yduration" variables that's not already taken
+    xdur <- "xduration"
+    while(xdur%in% names(x)|xdur%in% names(y)){
+      xdur <- paste0("i.",xdur)
+    }
+    ydur <- "yduration"
+    while(ydur%in% names(x)|ydur%in% names(y)){
+      ydur <- paste0("i.",ydur)
+    }
+    
+
+  #make a copy of y so a new column doesn't get created in the user's data.table
+  y <- copy(y)
   
-  print(paste(Sys.time(),"begin setting final x keys"))
-  if(any(group_vars %in% names(y))){
-    setkeyv(x,c(intersect(group_vars,names(y)), interval_vars))
-  }else{
-    setkeyv(x,interval_vars)
-  }
-  print(paste(Sys.time(),"setting final x keys complete"))
-  
-  
-  
-  stopifnot(!"yduration"%in% c(names(y),names(x)))
-  
-  group_vars_y <- intersect(group_vars,names(y))
-  if(length(group_vars_y)==0) group_vars_y <- NULL
-  
-  print(paste(Sys.time(),"begin foverlaps"))
-  
-  #make sure the names start and end aren't already in y (other than interval_vars)
-  stopifnot(!any(setdiff(c("start","end"),interval_vars) %in%names(y)))
-  stopifnot(!any(setdiff(c("start","end"),interval_vars) %in%names(x)))
-  setnames(y, interval_vars, c("start","end"))
-  
-  #count length of each interval, within groups of y if y has groups
+  #count length of each interval,
   #this will be used to count percentage of observed time x has in intervals of y
-  y[,yduration:=as.numeric((end-start)+1),by=group_vars_y]
   
-  z <- foverlaps(x,y) #this will result in a data.table with two sets of start and end date columns
-   #the ones prefixed with i. are from x 
+  y[,(ydur):=as.numeric( (get(interval_vars[2])-get(interval_vars[1])) + 1)]
   
-  setnames(z, interval_vars, c("i.start","i.end"))
+  setkeyv(y,interval_vars)
+  setkeyv(x,interval_vars)
+  z <- foverlaps(x,y) 
   
-  print(paste(Sys.time(),"foverlaps complete"))
+  #calucate the dur of the overlap:
+  #the first of the two end dates minus the second of the two start dates plus one
+  z[, (dur):=as.numeric(pmin( get(xinterval_vars[2]), get(interval_vars[2]))-
+                               pmax(get(xinterval_vars[1]),get(interval_vars[1]))) + 1] 
+
   
-  stopifnot(!"zzziii_max_start"%in% names(z))
-  stopifnot(!"zzziii_min_end"%in% names(z))
+  #keying by interval_vars should be the same as keying by interval_vars[1]
+  #since there are assumed to be no duplicates in intervals in y within categories of group_vars
+  setkeyv(z,c(group_vars,interval_vars)) 
+  z[,(xdur):= sum(get(dur)),by=c(group_vars,interval_vars) ]
   
-  print(paste(Sys.time(),"begin pmin/pmax"))
-  z[, zzziii_max_start:=pmax(start, i.start)]
-  z[, zzziii_min_end:=pmin(end,i.end)]
-  print(paste(Sys.time(),"pmin/pmax complete"))
   
-  stopifnot(!"duration"%in% c(names(y),names(x)))
-  stopifnot(!"xduration"%in% c(names(y),names(x)))
   
-  z[, duration:=as.numeric((zzziii_min_end-zzziii_max_start)+1)] #length of each interval
-  
-  print(paste(Sys.time(),"begin set z key"))
-  setkeyv(z,c(group_vars,c("start","end")))
-  print(paste(Sys.time(),"set z key complete"))
-  
-  print(paste(Sys.time(),"begin xduration"))
-  z[,xduration:= sum(duration),by=c(group_vars,"start") ]
-  print(paste(Sys.time(),"xduration complete"))
-  
-  #time weighted average call:
-  print(paste(Sys.time(),"begin time-weighted averaging"))
-  
+  #time weighted averaging:
   #for all value vars, calculate time-weighted mean
-  tw <- paste0("list(",paste0(value_vars,"=sum(",value_vars,"*duration)/xduration[1]",collapse=","),")")
-  q <- z[,eval(parse(text=tw)),
-         by=c(group_vars,"start","end","xduration","yduration")]
-  print(paste(Sys.time(),"time-weighted averaging complete"))
+
+  #xdur and ydur provide no additional grouping here 
+   #since they were created as a single value within cateogires of interval_vars and group_vars
+  q <- z[,lapply(.SD,function(x){
+    sum(x*get(dur))/.BY[[xdur]]
+  }),
+         by=c(group_vars,interval_vars,xdur,ydur),
+         .SDcols=value_vars]
   
+  stopifnot(q[,all(xdur<=ydur)])
+
+  #remove averages with too few observations in period
+  q[q[,100*get(xdur)/get(ydur) <required_percentage],
+    (value_vars) :=NA]
   
-  print(paste(Sys.time(),"begin duration check"))
-  stopifnot(q[,all(xduration<=yduration)])
-  print(paste(Sys.time(),"duration check complete"))
-  
-  print(paste(Sys.time(),"begin removing durations below required_percentage"))
-  set(q,q[,which(100*xduration/yduration <required_percentage)],
-      j=value_vars,
-      value=NA)
-  print(paste(Sys.time(),"complete"))
-  
-  setnames(q, c("start","end"),interval_vars)
-  q
-  
+  q <- EVAL("q[order(",group_vars,",",interval_vars,")]")
+  setcolorder(q, c(group_vars,interval_vars,value_vars))
+  q[]
 }
 
+
+#slow but more likely to be right since it expands the data
+interval_weighted_avg_slow_f <- function(x, y,interval_vars,value_vars, group_vars=NULL,
+                                    required_percentage=100,skip_overlap_check=FALSE){
+  
+  t <- "time"
+  
+  while(t %in% names(x)|t%in%names(y))
+    t <- paste0("i.",t)
+  
+  EVAL <- function(...)eval(parse(text=paste0(...)))
+  x_expanded <- EVAL("x[,list(",t,"=",interval_vars[1],":",interval_vars[2],"),
+       by=c(group_vars,interval_vars,value_vars)]")
+  x_expanded[,(interval_vars):=NULL]
+  
+  y_expanded <-  EVAL("y[,list(",t,"=",interval_vars[1],":",interval_vars[2],"),
+       by=interval_vars]")
+  
+  #for every combination of group vars, create a row for each time in y
+  
+  
+  unique_id_list <- as.list(x[,lapply(.SD,unique),.SDcols=group_vars])
+  unique_time_list <- list(t=y_expanded[[t]])
+  
+  y_expanded_complete <- do.call("CJ", c(unique_time_list, unique_id_list ))
+  setnames(y_expanded_complete,"t",t)
+  
+  #merge back in start and end times
+  setkeyv(y_expanded_complete,t)
+  setkeyv(y_expanded,t)
+  y_expanded_complete <- y_expanded[y_expanded_complete]
+  
+  setkeyv(x_expanded,c(t, group_vars))
+  setkeyv(y_expanded_complete,c(t, group_vars))
+  
+  z <- x_expanded[y_expanded_complete]
+  as.data.frame(z[id==1&id2==1])
+  
+  avg_call <-
+    paste0("list(",paste0(value_vars,"=mean(",value_vars,")",collapse=","),")")
+  
+  out <- EVAL("z[,",avg_call ,",by=c(interval_vars,group_vars)]")
+  
+  
+  out <- EVAL("out[order(",group_vars,",",interval_vars,")]")
+  setcolorder(out, c(group_vars,interval_vars,value_vars))
+  out[]
+  
+  
+}
 
 remove_overlaps <- function(x){
   x <- copy(x)
